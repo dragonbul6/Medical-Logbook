0419c1c5d96a5a5ebff5375524a887b62b26dff3
Sirawhic Saiweaw <dragonbul6@gmail.com>
2020-02-22 11:47:18 +0700

    xxx

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,7 @@ module.exports = (app)=>{
 
     app.post(path+`login`,userController.Oauth);
     app.post(path+`create`,userController.onCreate);
-    app.get(path,userController.onReads);
+    app.get(path+`all`,userController.onReads);
     app.get(path,userController.onRead);
     app.put(path+'update',userController.onUpdate);
     app.delete(path+`delete`,userController.onDelete);
