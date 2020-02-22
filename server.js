66887364a9cff4fbae083f1f578ab06b1722ea39
Sirawhic Saiweaw <dragonbul6@gmail.com>
2020-02-22 12:42:08 +0700

    a

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express'); 
 const bodyParser = require('body-parser');
-const userRoutes = require('./routes/userRoutes')
 const dbConnection = require('./helper/db')
 const cors = require('cors')
 
+//static
 const PORT = process.env.PORT || 3000
 
 //Middleware
 const middleware = require('./helper/middleware/checkAuth')
 
-const app = express();
+//routes 
+const userRoutes = require('./routes/userRoutes')
+const hospitalRoutes = require('./routes/hospitalRoutes');
 
+
+const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
@@ -19,7 +23,9 @@ app.get('/',middleware.verifyToken,(req,res)=>{
  res.json({"msg" : "Authenticated"});
 });
 
-userRoutes(app)
+userRoutes(app);
+hospitalRoutes(app);
+
 dbConnection();
 
 app.listen(PORT, ()=>{ 
