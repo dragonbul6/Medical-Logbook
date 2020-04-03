20cbbeddb38f5f47f25e6d1383f96cb1e44a1e2b
dragonbul <dragonbul6@gmail.com>
2020-04-03 14:13:01 +0700

    a

--- app/models/post/postingModel.js
+++ app/models/post/postingModel.js
@@ -8,38 +8,9 @@ let PostingSchema = new Schema({
         name : {type:String,required:false},
         HN : {type:String,required:false},
         Age : {type:Number,required:false},
-        Nationality : {type:String,default:"-"},
-        Religion : {type:String,default:"-"},
-        Informant : {type:String,default:"-"},
-        Reliability : {type:String,default:"-"},
-        Complaint : {type:String,default:"-"},
-        now_illness : {type:String,default:"-"},
-        past_history : {type:String,default:"-"},
-        Family_history : {type:String,default:"-"}        
     },
-    Examination : {
-        currentTime : {type: Date,default: Date.now},
-        category : {
-            vitalsign : {
-                temp : {type:String,default:"-"},
-                pulse : {type:String,default:"-"},
-                bloodPressure :{type:String,default:"-"},
-                respiratory : {type:String,default:"-"},
-                weight : {type:Number,default:0},
-                height : {type:Number,default:0} 
-            },
-            systemic : {
-                head : {type:String,default:"-"},
-                cvs : {type:String,default:"-"},
-                abs : {type:String,default:"-"},
-                respiratory : {type:String,default:"-"},
-                msk : {type:String,default:"-"},
-                neuro : {type:String,default:"-"}
-            },
-            specialtest : {type:String,default:"-"},
-            investigate : {type:String,default:"-"}
-        }
-    },
+    Examination : {type:String,default:null},
+    currentTime : {type: Date,default: Date.now},
     Problem_Diagnosis : {
         Problem : {type:String,required:false},
         Diagnosis : {type:String,required:false}
